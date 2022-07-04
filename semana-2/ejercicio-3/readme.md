fef4f72701dfd30504eb089c021ace3154a628f0
Pablo Peña <insomnionebulous@gmail.com>
2022-07-04 16:10:24 -0300

    Exercise 3 finished

---
+++ semana-2/ejercicio-3/readme.md
@@ -0,0 +1,9 @@
+# Propuesta del ejercicio
+
+A continuación de tu CV realizado en la práctica anterior, agrega ahora más abajo una sección para contactarte, cómo se encuentra a continuación
+
+![Ejemplo](https://cdn.discordapp.com/attachments/989234766517792768/993380960521441330/html_6_practica_formulario.png)
+
+con las opciones para temática:
+
+![Ejemplo](https://cdn.discordapp.com/attachments/989234766517792768/993383612961525770/html_6_practica_formulario_2.png)
---
+++ semana-2/ejercicio-3/script.js
@@ -0,0 +1,23 @@
+const selectBoxPlaceholder = document.querySelector(".select-box__placeholder")
+const selectBoxItems = document.querySelectorAll(".select-box__item > label")
+
+selectBoxItems.forEach((item) => {
+	item.addEventListener("click", (e) => {
+		selectBoxPlaceholder.classList.contains("undefined") &&
+			selectBoxPlaceholder.classList.remove("undefined")
+
+		selectBoxPlaceholder.innerText = e.target.innerText
+	})
+})
+
+selectBoxPlaceholder.addEventListener("click", () => {
+	selectBoxPlaceholder.classList.toggle("active")
+	selectBoxPlaceholder.nextElementSibling.classList.toggle("hidden")
+})
+
+document.addEventListener("click", (e) => {
+	if (!e.target.closest(".select-box")) {
+		selectBoxPlaceholder.classList.remove("active")
+		selectBoxPlaceholder.nextElementSibling.classList.add("hidden")
+	}
+})
